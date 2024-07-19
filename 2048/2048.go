96abf0908a0213f063b76c2004bb072c8d70c5be
bpinheiro <bernardopinheiro19@gmail.com>
2024-07-19 02:32:46 +0100

    Implemented game functions and draw value text

--- 2048/2048.go
+++ 2048/2048.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"strconv"
 
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
 type Game struct {
@@ -27,6 +30,8 @@ const (
 	tileSize    = 150
 	tileMargin  = 160
 	tileSpacing = 50
+
+	fontSize = 90
 )
 
 var (
@@ -42,7 +47,49 @@ var (
 	COLOR512         = color.RGBA{245, 110, 70, 0xff}
 	COLOR1024        = color.RGBA{245, 80, 55, 0xff}
 	COLOR2048        = color.RGBA{245, 60, 40, 0xff}
+
+	mplusFaceSource *text.GoTextFaceSource
 )
+
+func newGame() *Game {
+	game := &Game{}
+	game.init()
+	game.initBoard()
+	game.printBoard()
+	return game
+}
+
+func (g *Game) init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	mplusFaceSource = s
+}
+
+func (g *Game) initBoard() {
+	g.tiles = make([]Tile, 16)
+	for i := 0; i < boardSize; i++ {
+		for n := 0; n < boardSize; n++ {
+			t := Tile{value: 2, x: i, y: n}
+			g.tiles[i+n] = t
+		}
+	}
+}
+
+func (g *Game) printBoard() {
+	fmt.Println(" ### 2048 Board ### ")
+	fmt.Println(" ----------------- ")
+	for i := 0; i < boardSize; i++ {
+		for n := 0; n < boardSize; n++ {
+			fmt.Print(" | ")
+			fmt.Print(g.tiles[i+n].value)
+
+		}
+		fmt.Println(" | ")
+	}
+	fmt.Println(" ----------------- ")
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -81,6 +128,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 				op.GeoM.Translate(float64(i*tileSize+tileSpacing+3), float64(n*tileSize+tileSpacing+3))
 				screen.DrawImage(tile, op)
 
+				optext := &text.DrawOptions{}
+				optext.GeoM.Translate(float64(i*tileSize+tileSpacing+45), float64(n*tileSize+tileSpacing+15))
+				optext.ColorScale.ScaleWithColor(color.Black)
+				text.Draw(screen, strconv.Itoa(g.tiles[i+n].value), &text.GoTextFace{
+					Source: mplusFaceSource,
+					Size:   float64(fontSize),
+				}, optext)
 			}
 		}
 	}
@@ -94,7 +148,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(ScreenHeight, ScreenWidth)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(newGame()); err != nil {
 		log.Fatal(err)
 	}
 }
