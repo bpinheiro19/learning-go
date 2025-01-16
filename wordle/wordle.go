efe34ccf93b0fd21a3a4720ec162c9a587df94ea
bpinheiro <bernardopinheiro19@gmail.com>
2025-01-16 23:39:41 +0000

    Wordle game initial commit

---
+++ wordle/wordle.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+)
+
+type Game struct {
+	mplusFaceSource *text.GoTextFaceSource
+	board           [][]byte
+	running         bool
+	gameover        bool
+	win             bool
+}
+
+const (
+	ScreenWidth  = 1000
+	ScreenHeight = 850
+	boardWidth   = 5
+	boardHeight  = 6
+)
+
+func newGame() *Game {
+	game := &Game{}
+	game.init()
+	game.running = true
+	game.gameover = false
+	game.win = false
+	return game
+}
+
+func (g *Game) init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.mplusFaceSource = s
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ScreenHeight, ScreenWidth
+}
+
+func main() {
+	ebiten.SetWindowSize(ScreenHeight, ScreenWidth)
+	ebiten.SetWindowTitle("Wordle")
+	if err := ebiten.RunGame(newGame()); err != nil {
+		log.Fatal(err)
+	}
+}
