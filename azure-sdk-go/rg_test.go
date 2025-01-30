9ec9d6b37d4be26cd30c82502a2b350312c39604
bpinheiro <bernardopinheiro19@gmail.com>
2025-01-30 23:59:29 +0000

    Add tests

---
+++ azure-sdk-go/rg_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreateResourceGroup(t *testing.T) {
+	subscriptionId := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	resourceGroupName := "aks-rg"
+	credential := getAzureCredential()
+
+	expectedResult := "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName
+
+	createResourceGroupClient(subscriptionId, credential)
+	createResourceGroup(resourceGroupName)
+
+	result := getResourceGroup(resourceGroupName)
+
+	if result != expectedResult {
+		t.Errorf("Result was incorrect, got: %s, wanted: %s.", result, expectedResult)
+	}
+}
